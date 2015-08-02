71b41272fd9b0eba030fa22fdbefc662488a9cd8
Liu Yang <zation1@gmail.com>
2015-08-02 18:02:31 +0800

    add back button

---
+++ components/back-button.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Reflux = require('reflux');
+var _ = require('lodash');
+var PropTypes = require('react/lib/ReactPropTypes');
+
+var RoutesStore = require('../stores/routes');
+var RoutesAction = require('../actions/routes');
+var Link = require('./link.jsx');
+
+module.exports = React.createClass({
+  propTypes: {
+    className: React.PropTypes.string
+  },
+
+  mixins: [Reflux.connect(RoutesStore, 'routes')],
+
+  back: function() {
+    RoutesAction.back();
+  },
+
+  render: function() {
+    return (
+      <Link className={this.props.className || 'btn btn-default'} onClick={this.back}>
+        {this.props.children || 'Back'}
+      </Link>
+    );
+  }
+});
