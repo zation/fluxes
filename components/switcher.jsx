224c01c6d1a5b294abd9fa50b2e5fdfa3aa57932
Liu Yang <zation1@gmail.com>
2015-08-07 22:21:39 +0800

    add a basic switcher

---
+++ components/switcher.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var _ = require('lodash');
+var classnames = require('classnames');
+
+module.exports = React.createClass({
+  propTypes: {
+    className: React.PropTypes.string,
+    type: React.PropTypes.oneOf(['round', 'flat'])
+  },
+
+  render: function() {
+    var id = 'a' + (new Date()).getTime().toString() + _.random(1000);
+
+    return (
+      <span className={this.props.className}>
+        <input id={id} type="checkbox" className={classnames(
+        'switcher',
+        this.props.type === 'flat' ? 'switcher-flat' : 'switcher-round'
+      )}/>
+        <label htmlFor={id}></label>
+      </span>
+    );
+  }
+});
