36ff9ee4e53defa1ef986d7de49550b0ca969561
Liu Yang <zation1@gmail.com>
2015-08-25 11:15:19 +0800

    hide backbutton when no route to back

--- components/back-button.jsx
+++ components/back-button.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
+var Reflux = require('reflux');
 
 var Router = require('../libs/router');
+var RoutesStore = require('../stores/routes');
 var Link = require('./link.jsx');
 
 module.exports = React.createClass({
@@ -8,11 +10,17 @@ module.exports = React.createClass({
     className: React.PropTypes.string
   },
 
+  mixins: [Reflux.connect(RoutesStore, 'routes')],
+
   back: function() {
     Router.back();
   },
 
   render: function() {
+    if (this.state.routes.length < 2) {
+      return null;
+    }
+
     return (
       <Link className={this.props.className || 'btn btn-default'} onClick={this.back}>
         {this.props.children || 'Back'}
